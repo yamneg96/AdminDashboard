c560b7b1cbc3665ac21b35bd97fae0b2fa30c58e
Yamlak Negash <yamlaknegash96@gmail.com>
2025-07-14 00:55:26 +0300

    Added and configured syncfusion license key

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,9 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+import {registerLicense} from "@syncfusion/ej2-base";
+registerLicense(import.meta.env.VITE_EJS_LICENSE_KEY);
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
