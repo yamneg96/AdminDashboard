e48124bfcc16fa7758c7623f46a63e3916ced313
Yamlak Negash <yamlaknegash96@gmail.com>
2025-07-14 00:22:59 +0300

    First Commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,8 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import React from 'react'
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+const Home = () => {
+    return (
+        <div>Home</div>
+    )
 }
-
-export default function Home() {
-  return <Welcome />;
-}
+export default Home
